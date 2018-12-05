55cb9e3254906b24e935c364fe617eeb1988fb41
Erik Weitenberg <eweitenberg@gmail.com>
2018-12-05 10:19:10 +0100

    solved 5

---
+++ 05/puzzle05/src/main.rs
@@ -0,0 +1,48 @@
+use std::io; // provides io's stdin()
+use std::io::BufRead;
+
+fn main() {
+    let stdin = io::stdin();
+    let elements: Vec<_> = stdin
+        .lock()
+        .lines()
+        .filter_map(Result::ok)
+        .next()
+        .expect("No input")
+        .as_str()
+        .chars()
+        .map(|c| c as u8)
+        .collect();
+
+    let lengths: Vec<_> = (0..26)
+        .map(|i| react(remove_elem(&elements, 'A' as u8 + i)))
+        .collect();
+
+    println!("{:?}", react(elements.clone()));
+    println!("{:?}", lengths.iter().min().unwrap());
+}
+
+const CAPITAL: u8 = ('a' as u8) - ('A' as u8);
+
+fn react(mut elements: Vec<u8>) -> usize {
+    let mut i = 0;
+    while i + 1 < elements.len() {
+        if elements[i] == elements[i + 1] + CAPITAL || elements[i] + CAPITAL == elements[i + 1] {
+            elements.drain(i..(i + 2));
+            if i > 0 {
+                i -= 1;
+            }
+        } else {
+            i += 1;
+        }
+    }
+
+    elements.len()
+}
+
+fn remove_elem(v: &Vec<u8>, elem: u8) -> Vec<u8> {
+    v.iter()
+        .filter(|&&n| n != elem && n != elem + CAPITAL)
+        .map(|&n| n)
+        .collect()
+}
