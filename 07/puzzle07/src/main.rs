c337df64b180ad25451d6994b63608283cdf729d
Erik Weitenberg <eweitenberg@gmail.com>
2018-12-07 12:24:14 +0100

    even more idiomatic

--- 07/puzzle07/src/main.rs
+++ 07/puzzle07/src/main.rs
@@ -12,14 +12,10 @@ use std::io::BufRead;
 use std::io::Error;
 
 fn main() {
-    let stdin = io::stdin();
-    let task_order: Vec<(char, char)> = stdin.lock().lines().map(|l| parse_line(l)).collect();
+    let task_order: Vec<(char, char)> = io::stdin().lock().lines().map(|l| parse_line(l)).collect();
 
-    let mut all: Vec<char> = CharRangeInclusive(
-        'A',
-        task_order.iter().fold('A', |m, (a, b)| max(m, max(*a, *b))),
-    )
-    .collect();
+    let highest = task_order.iter().fold('A', |m, (a, b)| max(m, max(*a, *b)));
+    let mut all: Vec<char> = CharRangeInclusive('A', highest).collect();
 
     // Part 1. Do the tasks in order
     let mut done = Vec::new();
@@ -38,26 +34,28 @@ fn main() {
     let mut time = 0;
 
     loop {
-        for w in 0..WORKERS {
-            if let Some(task) = workers[w].current {
-                workers[w].time_left -= 1;
-                if workers[w].time_left == 0 {
+        // Step all jobs, and free workers who are done
+        for w in workers.iter_mut() {
+            if let Some(task) = w.current {
+                w.time_left -= 1;
+                if w.time_left == 0 {
                     done.push(task);
-                    workers[w] = Default::default();
+                    *w = Default::default();
                 }
             }
         }
-        for w in 0..WORKERS {
-            if workers[w].current.is_none() {
-                if let Some(next) = available(&all, &done, &task_order) {
-                    workers[w] = Task {
-                        current: Some(next),
-                        time_left: (next as usize) - ('A' as usize) + EXTRA_TIME + 1,
-                    };
-                    all = all.into_iter().filter(|&t| t != next).collect();
-                }
+        // Give new jobs to idle workers
+        for w in workers.iter_mut().filter(|w| w.current.is_none()) {
+            if let Some(next_task) = available(&all, &done, &task_order) {
+                *w = Task {
+                    current: Some(next_task),
+                    time_left: (next_task as usize) - ('A' as usize) + EXTRA_TIME + 1,
+                };
+                // Remove the task from the pile of stuff to do
+                all = all.into_iter().filter(|&t| t != next_task).collect();
             }
         }
+        // If everyone is still idle, we're done
         if workers.iter().filter(|w| w.current.is_some()).count() == 0 {
             break;
         }
