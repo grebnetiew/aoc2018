ec55840378c64e74efcbea78b69a7823503322e8
Erik Weitenberg <eweitenberg@gmail.com>
2018-12-08 13:59:07 +0100

    slightly improved my impl version, also fixed underflow

--- 08/puzzle08/src/main.rs
+++ 08/puzzle08/src/main.rs
@@ -12,8 +12,8 @@ fn main() {
         .map(|s| s.parse().expect("Error: That wasn't a number"))
         .collect();
 
-    println!("{:?}", take_metadata(&mut numbers.iter().map(|s| *s)));
-    println!("{:?}", sum_nodevalue(&mut numbers.iter().map(|s| *s)));
+    println!("{:?}", take_metadata(&mut numbers.iter()));
+    println!("{:?}", sum_nodevalue(&mut numbers.iter()));
 }
 
 // For my own purposes of learning the language, these functions each take an
@@ -22,18 +22,14 @@ fn main() {
 // instantiates it for the type I will actually feed to it (likely std::iter::Map).
 // This performs faster than the alternative, but takes more space if you have many
 // kinds of iterators.
-// Using an Item = &usize, which would be more elegant, leads to a headache with
-// lifetimes I have not been able to solve (because of the recursive call).
+// You don't see the generic type T, as "impl" is syntactic sugar for that.
 
-fn take_metadata<T>(iter: &mut T) -> usize
-where
-    T: Iterator<Item = usize>,
-{
-    let num_nodes = iter.next().unwrap();
-    let num_mdata = iter.next().unwrap();
+fn take_metadata<'a>(iter: &mut impl Iterator<Item = &'a usize>) -> usize {
+    let num_nodes = *iter.next().unwrap();
+    let num_mdata = *iter.next().unwrap();
 
     let total_meta: usize = (0..num_nodes).map(|_| take_metadata(iter)).sum();
-    total_meta + iter.take(num_mdata).sum::<usize>()
+    total_meta + iter.take(num_mdata).map(|s| *s).sum::<usize>()
 }
 
 // This function takes a "dyn" iterator, which Rust calls a Trait object (cf "impl"
@@ -41,19 +37,18 @@ where
 // in C++: results in only one function sum_nodevalue, and .next checks at runtime
 // what type your iterator is and which function .next it should call. Takes some time,
 // saves space if you have many kinds of iterators.
-// Typing "impl" instead of "dyn" fails catastrophically. I'm not sure why.
 
-fn sum_nodevalue(iter: &mut dyn Iterator<Item = usize>) -> usize {
-    let num_nodes = iter.next().unwrap();
-    let num_mdata = iter.next().unwrap();
+fn sum_nodevalue(iter: &mut dyn Iterator<Item = &usize>) -> usize {
+    let num_nodes = *iter.next().unwrap();
+    let num_mdata = *iter.next().unwrap();
 
     if num_nodes == 0 {
-        return iter.take(num_mdata).sum();
+        return iter.take(num_mdata).map(|s| *s).sum();
     }
 
     let child_values: Vec<_> = (0..num_nodes).map(|_| sum_nodevalue(iter)).collect();
     iter.take(num_mdata)
-        .filter(|&index| index <= num_nodes)
-        .map(|index| child_values[index - 1])
+        .filter(|&&index| index <= num_nodes)
+        .map(|&index| child_values[index.wrapping_sub(1)])
         .sum()
 }
