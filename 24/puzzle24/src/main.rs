cbdec69d2cd82b2dbc697f9fd846824d2ca87e33
Erik Weitenberg <eweitenberg@gmail.com>
2018-12-25 16:00:24 +0100

    refactoring solution to 24

--- 24/puzzle24/src/main.rs
+++ 24/puzzle24/src/main.rs
@@ -4,60 +4,53 @@ use regex::Regex;
 use std::fmt;
 use std::io;
 use std::io::BufRead;
+use std::ops::Deref;
+use std::ops::DerefMut;
 
 fn main() {
     let stdin = io::stdin();
     let mut lines = stdin.lock().lines().filter_map(Result::ok);
-    lines.next(); // skip 1st heading
 
     let mut army_groups = Vec::new();
+    let mut current_team = Team::Immunity;
     while let Some(l) = lines.next() {
-        if let Some(army) = parse_line(&l, Team::Immunity) {
+        if let Some(army) = parse_line(&l, current_team) {
             army_groups.push(army);
-        } else {
-            break;
-        }
-    }
-    while let Some(l) = lines.next() {
-        if let Some(army) = parse_line(&l, Team::Infection) {
-            army_groups.push(army);
+        } else if l.starts_with("Infection") {
+            current_team = Team::Infection;
         }
     }
 
     // Part 1
-    let mut part1_armies = army_groups.clone();
-    while winner(&part1_armies).is_none() {
-        play_round(&mut part1_armies);
-        part1_armies.retain(|a| a.units != 0);
+    let mut part1 = Battlefield(army_groups.clone());
+    while part1.winner().is_none() {
+        part1.play_round();
     }
 
     println!(
         "Winning army has {} units left",
-        part1_armies.iter().map(|a| a.units).sum::<u32>()
+        part1.iter().map(|a| a.units).sum::<u32>()
     );
 
     // Part 2
-    let mut boost_amount = 0;
-    loop {
-        boost_amount += 1;
-        let mut part2_armies = army_groups.clone();
-        for a in part2_armies.iter_mut().filter(|a| a.team == Team::Immunity) {
+    for boost_amount in 1.. {
+        let mut part2 = Battlefield(army_groups.clone());
+        for a in part2.iter_mut().filter(|a| a.team == Team::Immunity) {
             a.damage += boost_amount;
         }
-        while winner(&part2_armies).is_none() {
-            let something_happened = play_round(&mut part2_armies);
+        while part2.winner().is_none() {
+            let something_happened = part2.play_round();
             if !something_happened {
                 // It's a stalemate
                 break;
             }
-            part2_armies.retain(|a| a.units != 0);
         }
 
-        if winner(&part2_armies) == Some(Team::Immunity) {
+        if part2.winner() == Some(Team::Immunity) {
             println!(
                 "Immunity first wins at boost amount {} and has {} units left",
                 boost_amount,
-                part2_armies.iter().map(|a| a.units).sum::<u32>()
+                part2.iter().map(|a| a.units).sum::<u32>()
             );
             return;
         }
@@ -66,8 +59,8 @@ fn main() {
 
 fn parse_line(line: &String, team: Team) -> Option<ArmyGroup> {
     lazy_static! {
-        static ref re_group: Regex = Regex::new(r"(\d+) units each with (\d+) hit points (\([a-z ;,]*\) )?with an attack that does (\d+) (\w+) damage at initiative (\d+)").unwrap();
-        static ref re_weak: Regex = Regex::new(r"(weak|immune) to (\w+)(, (\w+))?").unwrap();
+        static ref re_group: Regex = Regex::new(r"(\d+) units each with (\d+) hit points (\([\w ;,]*\) )?with an attack that does (\d+) (\w+) damage at initiative (\d+)").unwrap();
+        static ref re_weak: Regex = Regex::new(r"(weak|immune) to ([\w, ]+)").unwrap();
     }
 
     re_group.captures(line).and_then(|caps| {
@@ -82,89 +75,113 @@ fn parse_line(line: &String, team: Team) -> Option<ArmyGroup> {
             immunities: Vec::new(),
         };
         for caps in re_weak.captures_iter(line) {
-            let mut kinds = vec![caps[2].to_owned()];
-            if let Some(kind) = caps.get(4) {
-                kinds.push(kind.as_str().to_owned());
-            }
+            let mut kinds = caps[2].split(", ").map(str::to_owned).collect();
             match &caps[1] {
                 "weak" => army_group.weaknesses.append(&mut kinds),
                 "immune" => army_group.immunities.append(&mut kinds),
-                _ => panic!("Unknown immunity modifier"),
+                _ => panic!("This match of (weak|immune) matched neither?!"),
             }
         }
         Some(army_group)
     })
 }
 
-fn play_round(armies: &mut Vec<ArmyGroup>) -> bool {
-    // First we select targets
-    let mut order: Vec<usize> = (0..armies.len()).collect();
-    order.sort_by_key(|&i| (armies[i].effective_power(), armies[i].initiative));
-    order.reverse();
+#[derive(Debug)]
+struct Battlefield(Vec<ArmyGroup>);
 
-    // For each army, targets[i] is its target
-    let mut targets: Vec<Option<usize>> = vec![None; armies.len()];
-    // For each army, targeted_by[i] is the army targeting it
-    let mut targeted_by: Vec<Option<usize>> = vec![None; armies.len()];
-
-    for i in order.iter() {
-        // army i chooses a target
-        if let Some(target) = armies
-            .iter()
-            .enumerate()
-            .filter(|(idx, candidate)| {
-                candidate.team != armies[*i].team
-                    && targeted_by[*idx].is_none()
-                    && armies[*i].possible_damage(candidate) != 0
-            })
-            .max_by_key(|(_, candidate)| {
-                (
-                    armies[*i].possible_damage(candidate),
-                    candidate.effective_power(),
-                    candidate.initiative,
-                )
-            })
-        {
-            targets[*i] = Some(target.0);
-            targeted_by[target.0] = Some(*i);
-        }
+// By implementing Deref(Mut), we ensure that Vec methods like .iter() and [] still work
+impl Deref for Battlefield {
+    type Target = Vec<ArmyGroup>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
-
-    // Now we do damage
-    let mut order: Vec<usize> = (0..armies.len()).collect();
-    order.sort_by_key(|&i| std::u32::MAX - armies[i].initiative);
-
-    let mut something_happened = false;
-
-    for i in order.iter() {
-        if armies[*i].units == 0 {
-            continue;
-        }
-        if let Some(target) = targets[*i] {
-            let dmg = armies[*i].possible_damage(&armies[target]);
-            let taken_damage = armies[target].take_damage(dmg);
-            something_happened |= taken_damage;
-        }
+}
+impl DerefMut for Battlefield {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
     }
-
-    something_happened
 }
 
-fn winner(armies: &Vec<ArmyGroup>) -> Option<Team> {
-    for i in 1..armies.len() {
-        if armies[i].team != armies[0].team {
-            return None;
-        }
-    }
-    Some(armies[0].team)
-}
+impl Battlefield {
+    fn play_round(&mut self) -> bool {
+        // First we select targets
+        let mut order: Vec<usize> = (0..self.len()).collect();
+        // Groups select targets in decreasing order of effective power, then initiative
+        order.sort_by_key(|&i| (self[i].effective_power(), self[i].initiative));
+        order.reverse();
 
-#[allow(dead_code)]
-fn print_all(armies: &Vec<ArmyGroup>) {
-    for a in armies.iter() {
-        println!("{}", a);
+        // For each group, targets[i] is its target
+        let mut targets: Vec<Option<usize>> = vec![None; self.len()];
+        // For each group, targeted_by[i] is the group targeting it
+        let mut targeted_by: Vec<Option<usize>> = vec![None; self.len()];
+
+        for &i in order.iter() {
+            // Group i chooses a target ...
+            if let Some(target) = self
+                .iter()
+                .enumerate()
+                .filter(|&(idx, candidate)| {
+                    // ... considering only untargeted groups from the opposing team that they can damage
+                    candidate.team != self[i].team
+                        && targeted_by[idx].is_none()
+                        && self[i].possible_damage(candidate) != 0
+                })
+                .max_by_key(|(_, candidate)| {
+                    // ... selecting the one they can do the most damage to,
+                    // breaking ties by selecting the one with highest power, then initiative
+                    (
+                        self[i].possible_damage(candidate),
+                        candidate.effective_power(),
+                        candidate.initiative,
+                    )
+                })
+            {
+                targets[i] = Some(target.0);
+                targeted_by[target.0] = Some(i);
+            }
+        }
+
+        // Now we do damage
+        let mut order: Vec<usize> = (0..self.len()).collect();
+        // Groups do damage in decreasing order of initiative
+        order.sort_by_key(|&i| std::u32::MAX - self[i].initiative);
+
+        let mut something_happened = false;
+
+        for &i in order.iter() {
+            // Dead groups do no damage
+            if self[i].units == 0 {
+                continue;
+            }
+            if let Some(target) = targets[i] {
+                let dmg = self[i].possible_damage(&self[target]);
+                let taken_damage = self[target].take_damage(dmg);
+                something_happened |= taken_damage;
+            }
+        }
+
+        // Remove any defeated groups
+        self.retain(|grp| grp.units != 0);
+
+        something_happened
     }
-    println!();
+
+    fn winner(&self) -> Option<Team> {
+        for i in 1..self.0.len() {
+            if self.0[i].team != self.0[0].team {
+                return None;
+            }
+        }
+        Some(self.0[0].team)
+    }
+
+    #[allow(dead_code)]
+    fn print_all(&self) {
+        for a in self.0.iter() {
+            println!("{}", a);
+        }
+        println!();
+    }
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
